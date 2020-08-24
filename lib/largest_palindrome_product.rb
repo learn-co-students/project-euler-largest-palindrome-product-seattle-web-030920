a05344410f248687cf082294034d797ee2cdda3e
fosterv2 <fosterv2@outlook.com>
2020-08-24 13:24:20 -0700

    Done.

--- lib/largest_palindrome_product.rb
+++ lib/largest_palindrome_product.rb
@@ -1,1 +1,17 @@
-# Implement your procedural solution here!
+# Implement your procedurxl solution here!
+def is_palindrome?(num)
+    num.to_s.reverse.to_i == num
+end
+
+def largest_palindrome_product
+    max = 0
+    (100..999).each do |first|
+      (100..999).each do |second|
+        product = first * second
+        if is_palindrome?(product) && product > max
+          max = product
+        end
+      end
+    end
+    max
+end
--- lib/oo_largest_palindrome_product.rb
+++ lib/oo_largest_palindrome_product.rb
@@ -1,1 +1,15 @@
 # Implement your object-oriented solution here!
+class LargestPalindromeProduct
+    def answer
+        max = 0
+        (100..999).each do |first|
+            (100..999).each do |second|
+                product = first * second
+                if product.to_s.reverse.to_i == product && product > max
+                    max = product
+                end
+            end
+        end
+        max
+    end
+end
